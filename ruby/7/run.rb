208da138bbfc5563c16cbd5f3574d64638bf4749
asmand <asmand@gmail.com>
2024-12-15 03:11:05 +0200

    Small cleanup

--- ruby/7/run.rb
+++ ruby/7/run.rb
@@ -11,60 +11,32 @@ class Integer
   end
 end
 
-def ops_1
-  %i[+ *]
-end
-
-def ops_2
-  %i[+ * con]
-end
-
-def find_sp1(sum, current, terms)
-  ops_1.each do |op|
+def find_sp(ops, sum, current, terms)
+  ops.each do |op|
     if terms.empty?
       raise if current == sum
     else
-      find_sp1(sum, current.send(op, terms[0]), terms[1..-1])
+      find_sp(ops, sum, current.send(op, terms[0]), terms[1..-1])
     end
   end
 end
 
-def find_sp2(sum, current, terms)
-  ops_2.each do |op|
-    if terms.empty?
-      raise if current == sum
-    else
-      find_sp2(sum, current.send(op, terms[0]), terms[1..-1])
+def sum_operations(ops, lines)
+  res = lines.sum do |line|
+    sum, terms = line.split(':')
+    sum = sum.to_i
+    terms = terms.split(' ').map(&:to_i)
+    begin
+      find_sp(ops, sum, terms[0], terms[1..-1])
+      0
+    rescue 
+      sum
     end
   end
 end
 
-res = lines.sum do |line|
-  sum, terms = line.split(':')
-  sum = sum.to_i
-  terms = terms.split(' ').map(&:to_i)
-  begin
-    find_sp1(sum, terms[0], terms[1..-1])
-    0
-  rescue 
-    sum
-  end
-end
+puts "with first set of operators: #{sum_operations(%i[+ *], lines)}"
 
-puts "with first set of operators: #{res}"
-
-res2 = lines.sum do |line|
-  sum, terms = line.split(':')
-  sum = sum.to_i
-  terms = terms.split(' ').map(&:to_i)
-  begin
-    find_sp2(sum, terms[0], terms[1..-1])
-    0
-  rescue 
-    sum
-  end
-end
-
-puts "with second set of operators: #{res2}"
+puts "with second set of operators: #{sum_operations(%i[+ * con], lines)}"
 
 
